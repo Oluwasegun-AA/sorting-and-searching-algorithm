43ec6f7b2c1c4ad252cfcf862ac99ec69b05e17c
shegsteham <oluwasegunadepoju@gmail.com>
2019-06-24 14:28:28 +0200

    fix(): Update project files
    - update UI
    - adopt es6 in .js file
    - update favicon
    - fix bugs in the sort_words function
    - adopt develop branch
    
    [finishes]

--- Js/Script.js
+++ Js/Script.js
@@ -6,23 +6,23 @@ var output = document.getElementById("output");
 //Declaring Global Variables
 
 //Event Listerner to Initiate the Check Function When button is clicked
-submit.addEventListener("click", check);
+submit.addEventListener("click", () => check());
 
 /**
  * @description the function checks for duplicated words in the string,
  * it retains only one of those words in the string, then calls the sort_words()
  * function which compares the letters in the words.
  */
-function check() {
-  var new_string = "";
-  var duplicates = "";
-  input_string = input.value;
+check = () => {
+  let new_string = "";
+  let duplicates = "";
+  let input_string = input.value;
 
   //replacing punctuations with white spaces
   input_string = input_string.replace(/[^A-Za-z\s]/g, "");
   var input_string_array = input_string.split(" ");
 
-  //checks through the Input String and removes all duplicates, retaining a single occurence of such duplicated word
+  //checks through the Input String and removes all duplicates, retaining a single occurrence of such duplicated word
   for (let i = 0; i < input_string_array.length; i++) {
     if (duplicates.search(input_string_array[i]) == -1) {
       if (
@@ -45,41 +45,46 @@ function check() {
  * has the same letters but arranged differently
  * @param {*} duplicate_void
  */
-function sort_words(duplicate_void) {
-  var count = 0;
-  var same_letters = "";
+sort_words = (duplicate_void) => {
+  let same_letters = "";
   output.style.display = "none";
   output.innerHTML = "";
 
   //converts the input void of duplicates into an array
-  var duplicate_void_array = duplicate_void.split(" ");
+  const duplicate_void_array = duplicate_void.split(" ");
   var array_length = duplicate_void_array.length;
 
   //checks through the array comparing each word to those after it
   for (let i = 0; i < array_length; i++) {
+    let count = 0;
     for (let j = i + 1; j < array_length; j++) {
+      count = 0;
       //checks to ensure that only words with equal length are compared in order to reduce processing time
-      if (duplicate_void_array[i].length == duplicate_void_array[j].length) {
+      if (duplicate_void_array[i].length === duplicate_void_array[j].length) {
         //compares two words alphabet by alphabet while count is incremented each time a letter is found in both words
         for (let k = 0; k < duplicate_void_array[i].length; k++) {
-          if (
+          if ((
             duplicate_void_array[j].indexOf(
               duplicate_void_array[i].charAt(k)
-            ) != -1
+            ) !== -1) && (
+              duplicate_void_array[i].indexOf(
+                duplicate_void_array[j].charAt(k)
+              ) !== -1)
           ) {
             count++;
           } else count = 0;
 
           //both words are saved when count equals the length of both words
-          if (count == duplicate_void_array[i].length) {
+          if (count === duplicate_void_array[i].length) {
             same_letters +=
-              '<p> " <strong>' +
-              duplicate_void_array[i] +
-              '</strong>" \xa0 and \xa0 "<strong>' +
-              duplicate_void_array[j] +
-              '</strong>" \xa0  contains the same letters </p>';
+              `<p> <strong>
+                ${duplicate_void_array[i]}
+                </strong> \xa0 and \xa0 <strong>
+                ${duplicate_void_array[j]}
+                </strong> \xa0  contains the same letters </p>`;
           }
         }
+        count = 0
       }
     }
   }
